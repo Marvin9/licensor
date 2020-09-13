605e48002b03b97b4a002e6e5df1df12d60120f8
Marvin9 <marvinduff97@gmail.com>
2020-09-13 20:08:01 +0530

    chore: Terminal display process

--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"time"
 
 	"github.com/Marvin9/licensor/steps"
 )
 
 func main() {
+	// find . | grep -i "\(\.go\|\.sh\)$" | wc -l
+	fmt.Print("Working")
+	go (func() {
+		for {
+			for i := 0; i < 3; i++ {
+				fmt.Print(".")
+				time.Sleep(time.Millisecond * 500)
+			}
+			for i := 0; i < 3; i++ {
+				fmt.Print("\b \b")
+			}
+		}
+	})()
 	var model steps.CommandModel
 	model.MakeModel(os.Args)
 	model.Validate()
@@ -18,4 +33,6 @@ func main() {
 	}
 
 	model.Start()
+	fmt.Print("\r          \r")
+	fmt.Println("✔️")
 }
